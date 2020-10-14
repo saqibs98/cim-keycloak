b3ee303e6f09998d35e6dfb2c9bb298a379fb578
your_username <saqib.hasanie@expertflow.com>
2020-10-14 14:50:29 +0500

    dynamic paths configured

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//require('environmental_variables').config();
 var express = require('express');
 var session = require('express-session');
 var bodyParser = require('body-parser');
@@ -27,25 +26,17 @@ app.use( keycloak.middleware() )
     }));
 
 app.use(bodyParser.json());
-  /*
-  module.exports.log = function (msg) { 
-    console.log(msg);
-  };*/
 
-  //module.exports.log = function () { 
-
-  
-    const userAuthentication = () => {
-      //----------------------------
+    const userAuthentication = (user_name,user_password) => {
 
       return new Promise((resolve, reject) => {
 
         const requestBody = {
-          username: process.env.username,// || "agent1",
-          password: process.env.password || "agent1",
-          client_id: process.env.CLIENT_ID || "Bank",
-          client_secret: process.env.CLIENT_SECRET || '2e991b1c-d340-437d-91a2-620465c51a4e',
-          grant_type: process.env.GRANT_TYPE || 'password'
+          username: user_name ,
+          password: user_password ,
+          client_id: process.env.CLIENT_ID,
+          client_secret: process.env.CLIENT_SECRET,
+          grant_type: process.env.GRANT_TYPE,          
         }
         var config = {
           headers: {
@@ -53,17 +44,22 @@ app.use(bodyParser.json());
             'Accept': 'application/json'
           }
         }
-        //----------------------------
-        let token;
-        ///// token request 1
-      
-         axios.post('http://192.168.1.47:8080/auth/realms/university/protocol/openid-connect/token', qs.stringify(requestBody), config)
+        
+      const x={
+        realm_name : process.env.REALM,
+        port: process.env.PORT,
+        host: process.env.HOST
+      }
+      const URL =  'http://'+x.host+':'+x.port+'/auth/realms/'+x.realm_name+'/protocol/openid-connect/token'
+
+        //  T.O.K.E.N   R.E.Q.U.E.S.T   # 1   ( P.E.R.M.I.S.S.I.O.N.S   N.O.T    I.N.C.L.U.D.E.D)   
+        
+        axios.post(URL, qs.stringify(requestBody), config)
           .then((response) => {
-            //console.log("/////Access Token: "+(response.data.access_token));
             if(response.data.access_token){
-              var requestBody2 = {
+              var requestBody1 = {
                 grant_type: "urn:ietf:params:oauth:grant-type:uma-ticket",
-                audience: "Bank",//process.env.CLIENT_ID || "Bank",
+                audience: process.env.CLIENT_ID
               }
               token = response.data.access_token;
               
@@ -74,15 +70,17 @@ app.use(bodyParser.json());
                   'Authorization' : 'Bearer '+ token,
                 }
               }
-        ///// RPT request 
-              axios.post('http://192.168.1.47:8080/auth/realms/university/protocol/openid-connect/token', qs.stringify(requestBody2), config1)
-              .then((responses) => {
+
+        //  T.O.K.E.N   R.E.Q.U.E.S.T   # 2   (A.C.C.E.S.S   T.O.K.E.N   W.I.T.H   P.E.R.M.I.S.S.I.O.N.S)    
+
+            axios.post(URL, qs.stringify(requestBody1), config1)
+                  .then((responses) => {
                 token = responses.data.access_token;
-                var requestBody1 = {
-                  client_id: process.env.CLIENT_ID || "Bank",
-                  client_secret: process.env.CLIENT_SECRET || '2e991b1c-d340-437d-91a2-620465c51a4e',
+                var requestBody2 = {
+                  client_id: process.env.CLIENT_ID,
+                  client_secret: process.env.CLIENT_SECRET, 
                   token: token,
-                  grant_type: 'password',
+                  grant_type: process.env.GRANT_TYPE 
                 }
                 const config3 = {
                   headers: {
@@ -90,44 +88,37 @@ app.use(bodyParser.json());
                     'Accept': 'application/json'
                   }
                 }
-                //------------- INTROSPECTION
-                    axios.post('http://192.168.1.47:8080/auth/realms/university/protocol/openid-connect/token/introspect', qs.stringify(requestBody1), config3)
+        //    I.N.T.R.O.S.P.E.C.T.I.O.N
+                    axios.post(URL+'/introspect', qs.stringify(requestBody2), config3)
                     .then((responsess) => {
                       responsess.data.access_token=token;
-                      //console.log("/////Introspection ",responses.data.access_token);
-                      resolve(responses.data.access_token);
-                      //callback(responsess);
-                     // res.send({data:responses.data});
+                     // console.log("/////Introspection ",responsess);
+                      resolve(responsess);
+
                     })
                     .catch((er) => {
                       reject(er);
-                      //res.status(err.response.status).send({ error: "------------Intro Error--------"});
                     })
-      
-              // responses.data.access_token=token;
-                //console.log("///// RPT  ",responses);
-                //res.send({data:responses.data});
-                //return responses.data;
               })
               .catch((er) => {
                 reject(er);
-                //res.status(err.response.status).send({ error: "-----RPT ERROR-----" });
               })
       
             }
           })
           .catch((err) => {
             reject(err);
-            //res.status(err.response.status).send({ error: "T---------token not found" });
           })
         });
       }
  
- 
       module.exports= {userAuthentication};
+
+
+      // F.U.N.C.T.I.O.N   C.A.L.L.I.N.G
 /*
-  userAuthentication().then((e)=>{
-    console.log("result : ///// " + e);
+  userAuthentication('agent1','agent1').then((e)=>{
+    console.log("result : ///// " + e.data);
   }).catch( (er)=>{
     console.log("reject error : " + er);
   })
